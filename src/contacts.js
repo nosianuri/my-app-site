3ad45222bf161945fd4b73962f26c4f6a53e4ecc
nosianuri <nurinosia@gmail.com>
2022-04-17 01:51:21 +0600

    added components contacts

---
+++ src/contacts.js
@@ -0,0 +1,23 @@
+const contacts = [
+    {
+        name: "Suzy Bae",
+        imgURL: "https://static.toiimg.com/photo/msid-89476950/89476950.jpg?24094",
+        phone: "+123 456 789",
+        email: "b@suzybae.com"
+    },
+    {
+        name: "Kim SeokJin",
+        imgURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTt5H2Qn3VJo5jF7EnfrynnASIPoUPgJtBu1_eIcQIWeVjRQ6S77D6KkaaZTjhA5Fe_fiM&usqp=CAU",
+        phone: "+987 654 321",
+        email: "kim@seokjin.com"
+    },
+    {
+        name: "Min Yongie",
+        imgURL: "https://stickers.wiki/static/stickers/btsworld/file_533922.webp?ezimgfmt=rs:134x134/rscb1/ng:webp/ngcb1",
+        phone : "+456 789 321",
+        email: "min@yongie.com"
+    }
+    
+];
+
+export default contacts;
